ec97687b1ba955e68e73d92405ebb7c38a681b2d
sztan <stanislaspaliard@gmail.com>
2017-10-16 23:47:25 +0200

    added bootstrap-vue (plus required style-loader and css-loader) as per following recommendations : https://bootstrap-vue.js.org/docs/ and https://github.com/webpack-contrib/style-loader

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import Offers from './Offers.vue'
+import BootstrapVue from 'bootstrap-vue'
 
 new Vue({
   el: '#app',
   render: h => h(Offers)
 })
+
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
